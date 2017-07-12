315dbf1d6056b83d1b457ac6ff790f4834a47b91
Irena Milinkovic <irena.milinkovic@mag-global.com>
2017-07-12 23:17:24 +0200

    Defined routes for select and insert reservation

--- src/clj/roombooker2/routes/home.clj
+++ src/clj/roombooker2/routes/home.clj
@@ -1,17 +1,33 @@
 (ns roombooker2.routes.home
   (:require [roombooker2.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [roombooker.db.core :as db]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html" {:reservations (db/get-all-reservations)}))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn save-reservation! [request]
+                         (db/save-reservation! (:params request) )
+                         (response/found "/"))
+
+(defn addReservation-page []
+    (layout/render "addReservation.html"))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (POST "/reservation" request (save-reservation! request))
   (GET "/about" [] (about-page)))
+
+
+
+
+
+
+
+
 
